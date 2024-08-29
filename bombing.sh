1b3b2312d2b6c7517c9d395189b68b18965323c6
Bappa Kumar <tanmoy760@gmail.com>
2024-08-28 23:53:28 -0700

    Update bombing.sh

--- bombing.sh
+++ bombing.sh
@@ -6,33 +6,33 @@ fi
 clear
 
 banner() {
-  echo -e 'SMS BOMBER'
+  printf '*** SMS BOMBER ***'
 }
 
 kp() {
-  echo -e "\e[95m Setting Up Server"
-  echo ""
-  echo -e "\e[92m[\e[91m1\e[92m]\e[93m SERVER 1\e[93m"
-  echo -e "\e[92m[\e[91m2\e[92m]\e[93m SERVER 2\e[93m"
-  echo -ne "\e[92mSelect Option\e[0m: \e[92m"
+  printf "\e[95m Setting Up Server\n"
+  printf "\n"
+  printf "\e[92m[\e[91m1\e[92m]\e[93m SERVER 1\e[93m\n"
+  printf "\e[92m[\e[91m2\e[92m]\e[93m SERVER 2\e[93m\n"
+  printf "\e[92mSelect Option\e[0m: \e[92m"
   read ch
   if [ $ch -eq 1 ]; then
     sp
   elif [ $ch -eq 2 ]; then
     sp1
   else
-    echo -e "\e[92mWrong Option Mere Bhai"
+    printf "\e[92mWrong Option Mere Bhai\n"
   fi
 }
 
 sp() {
   clear
   banner
-  echo -e "Start up Server"
+  printf "Start up Server\n"
   sleep 2.0
-  echo -e "\e[95m bombing on number :-$num"
+  printf "\e[95m bombing on number :-$num\n"
   sleep 2.0
-  echo -e "\e[91m bombing start"
+  printf "\e[91m bombing start\n"
   cp api1.sh app1.sh
   sed -i s/€tor/$num/g app1.sh
   bash app1.sh
@@ -42,11 +42,11 @@ sp() {
 sp1() {
   clear
   banner
-  echo -e "Start up Server"
+  printf "Start up Server\n"
   sleep 2.0
-  echo -e "\e[95m bombing on number :-$num"
+  printf "\e[95m bombing on number :-$num\n"
   sleep 2.0
-  echo -e "\e[91m bombing start"
+  printf "\e[91m bombing start\n"
   cp api.sh app.sh
   sed -i s/€tor/$num/g app.sh
   bash app.sh
@@ -54,7 +54,7 @@ sp1() {
 }
 
 banner
-echo -ne "\e[91m Enter Your number :-"
+printf "\e[91m Enter Mobile:"
 sleep 5.0
 
 bomb() {
@@ -64,21 +64,21 @@ bomb() {
   if [ "$method" == "true" ]; then
     kp
     method="false"
-    echo ""
+    printf "\n"
   else
     sp1
-    echo
+    printf "\n"
   fi
   con
 }
 
 con() {
-  echo -e " "
-  echo -e "\e[31m# \e[93m BOMBER\e[93m \e[92m(\e[31m$v\e[92m)"
-  echo -e "\e[92m[\e[91m1\e[92m]\e[93m RE-BOMB\e[93m"
-  echo -e "\e[92m[\e[91m2\e[92m]\e[93m EXIT\e[93m"
-  echo -e "\e[92m[\e[91m3\e[92m]\e[93m ARE BHAI BAS KAR PHONE MAAR JAYEGA USKA\e[93m"
-  echo -ne "select:-"
+  printf "\n"
+  printf "\e[31m# \e[93m BOMBER\e[93m \e[92m(\e[31m$v\e[92m)\n"
+  printf "\e[92m[\e[91m1\e[92m]\e[93m RE-BOMB\e[93m\n"
+  printf "\e[92m[\e[91m2\e[92m]\e[93m EXIT\e[93m\n"
+  printf "\e[92m[\e[91m3\e[92m]\e[93m ARE BHAI BAS KAR PHONE MAAR JAYEGA USKA\e[93m\n"
+  printf "Select:"
 
   read opt
   if [ $opt -eq 1 ]; then  
@@ -87,9 +87,9 @@ con() {
   elif [ $opt -eq 2 ]; then
     clear
   elif [ $opt -eq 3 ]; then
-    echo  -ne "\e[91m wrong input"
+    printf "\e[91m wrong input\n"
   else 
-    echo  ""
+    printf "\n"
   fi
 }
 
